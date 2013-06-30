a5367284e80d27fbc14a47a5a9f124e158d36a71
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-06-30 18:49:16 +0900

    device & typus設定導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ LinkStation::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.i18n.default_locale = :ja
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ LinkStation::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.i18n.default_locale = :ja
 end
