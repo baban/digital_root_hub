d5b50c74c3991f2003da09029e1070b6bf7f047f
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-06-30 19:05:10 +0900

    管理画面追加

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
   def index
-    @list = Author.page( params[:page] )
+    @list = Author.page( params[:page] ).order("name_kana DESC").per(20)
   end
 end
