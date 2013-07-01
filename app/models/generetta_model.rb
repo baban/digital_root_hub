2d404f63fd981bc6de23a17f1033537597761db5
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-07-01 20:53:45 +0900

    generetta

--- app/models/generetta_model.rb
+++ app/models/generetta_model.rb
@@ -1,3 +1,4 @@
 class GenerettaModel < ActiveRecord::Base
   attr_accessible :description
+  has_many :generetta_methods
 end
--- db/migrate/20130630104222_create_generetta_cases.rb
+++ db/migrate/20130630104222_create_generetta_cases.rb
@@ -2,8 +2,8 @@ class CreateGenerettaCases < ActiveRecord::Migration
   def change
     create_table :generetta_cases do |t|
       t.integer :generetta_method_id, null: false
-      t.string  :description
-      t.text    :content
+      t.string  :description,         null: false
+      t.text    :content,             null: true
       t.timestamps
     end
   end
