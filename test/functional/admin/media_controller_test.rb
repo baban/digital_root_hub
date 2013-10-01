10b337f5bb2c3211b34074e2db38cc28c87c3449
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-10-01 22:29:02 +0900

    管理画面の権限追加

--- test/functional/admin/media_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admin::MediaControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/functional/index_controller_test.rb
+++ test/functional/index_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class IndexControllerTest < ActionController::TestCase
+  describe "aaa" do
+    setup do
+      p :before
+    end
+
+    test "the truth" do
+      assert true
+    end
+
+    after do
+      p :after
+    end
+  end
   test "the truth" do
     assert true
   end
