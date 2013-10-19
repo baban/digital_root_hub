93c806b639668f1399825272f525fec246f0ff30
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-10-19 11:23:01 +0900

    検索モードを追加

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
   def index
-    @list = Author.visibles.includes(:sites).page( params[:page] ).order(:name_kana).per(20)
+    @list = Author.search( params ).page( params[:page] ).order(:name_kana).per(20)
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
 class Author < ActiveRecord::Base
   attr_accessible :name, :name_kana, :publiced, :memo
   attr_accessible :name, :name_kana, :publiced, :memo, as: :admin
   has_many :sites
   has_many :admin_operation_logs, as: :parent
 
-  scope :visibles, -> {where( publiced: true ) }
+  scope :visibles, -> { where( publiced: true ) }
+  # 作者名(A-Z)で検索する
+  scope :alphabets, ->( name ){ where( " name like ? ", "%#{name}%" ) }
+  # 作者名(あ-ん)で検索する
+  scope :kana, ->( name ){ where( " name like ? ", "%#{name}%" ) }
+
+  def self.search( prms )
+    rows = self.visibles.includes(:sites)
+    rows = case prms[:mode].to_i
+           when 1; rows.alphabets
+           when 2; rows.kana
+           else;   rows
+           end
+    rows
+  end
+
 end
